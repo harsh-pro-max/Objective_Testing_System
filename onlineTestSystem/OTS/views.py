070d0fa9316b1dffd381f5fd8e0eb1306fefff09
Harsh kushwaha <harsh90064@gmail.com>
2025-09-01 15:29:20 +0530

    project day1 first commit

---
+++ onlineTestSystem/OTS/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+# import all models in OTS 
+from OTS.models import *
+
+# Create your views here.
+
+def welcome(request):
+    pass
+
+def candidateRegistrationForm(request):
+    pass
+
+def cadidateRegistration(request):
+    pass
+
+def loginView(request):
+    pass
+
+def candidateHome(request):
+    pass
+
+def testPaper(request):
+    pass
+
+def claculateTestResult(request):
+    pass
+
+def testResultHistory(request):
+    pass
+
+def showTestResult(request):
+    pass
+
+def logoutView(request):
+    pass
+
